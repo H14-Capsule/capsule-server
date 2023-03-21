7d5b3e3cb8d05bdf9f80e95108e5a95ac4c904a5
GyeongrokKim <kr.kim@mfort.co.kr>
2023-03-22 00:03:25 +0900

    fix: Kotlin 런타임 클래스를 찾지 못해서 발생하는 문제 수정

--- hanghae-capsule/build.gradle.kts
+++ hanghae-capsule/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
