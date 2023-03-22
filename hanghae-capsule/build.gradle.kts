e3bd735c378d29e5c6b406a6a571fb19f4218338
GyeongrokKim <kr.kim@mfort.co.kr>
2023-03-22 22:57:43 +0900

    chore: postgreSQL 의존성 추가

--- hanghae-capsule/build.gradle.kts
+++ hanghae-capsule/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib")
+	implementation("org.postgresql:postgresql:42.5.0")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
